9df2c6f1af8db5a355056ff515922690915071a1
Deanna Nash <dnash@exp-18-58.expanse.sdsc.edu>
2023-12-15 12:03:02 -0800

    add trajectory updates

--- modules/trajectory.py
+++ modules/trajectory.py
@@ -8,6 +8,9 @@ import math
 import xarray as xr
 import pandas as pd
 import numpy as np
+import dask
+
+dask.config.set(**{'array.slicing.split_large_chunks': True})
 
 class calculate_backward_trajectory:
     '''
@@ -47,11 +50,15 @@ class calculate_backward_trajectory:
         start_date = ds.isel(date=idx).date.values - np.timedelta64(3,'D')
         end_date = ds.isel(date=idx).date.values
         self.date_lst_era = pd.date_range(start_date, end_date, freq='1D')
+
+    def preprocess(self, ds):
+        return ds.sel(latitude=slice(50., 15.), longitude=slice(-180., -80.))
     
     def read_data(self):    
         # read ERA5 data
         # create list of daily ERA5 files for each AR
-        ds_lst = []
+        # ds_lst = []
+        fname_lst = []
         for j, date in enumerate(self.date_lst_era):
             year = date.year
             month = date.strftime("%m")
@@ -59,12 +66,15 @@ class calculate_backward_trajectory:
             
             path_to_data = '/expanse/lustre/scratch/dnash/temp_project/downloaded/ERA5/{0}/'.format(year)
             fname = "era5_nhemi_025dg_1hr_uvwq_{0}{1}{2}.nc".format(year, month, day)
-            
-            ds2 = xr.open_dataset(path_to_data+fname)
-            ds_lst.append(ds2)
+            fname_lst.append(path_to_data+fname)
+            # ds2 = xr.open_dataset(path_to_data+fname)
+            # ds_lst.append(ds2)
         
         # concatenate ds_lst
-        self.ds1 = xr.concat(ds_lst, dim='time')
+        # self.ds1 = xr.concat(ds_lst, dim='time')
+
+        
+        self.ds1 = xr.open_mfdataset(fname_lst, engine='netcdf4', combine='by_coords', preprocess=self.preprocess)
 
         ## read MERRA2 data - not sure if we need this anymore
         # fname = '/data/downloaded/Reanalysis/MERRA2/M2I3NPASM.5.12.4_raw/1980/MERRA2_100.inst3_3d_asm_Np.19801231.nc4'
@@ -197,5 +207,3 @@ class calculate_backward_trajectory:
         self.df['drying_ratio'] = (self.df['dq']/self.df['q'])*100.# convert to %    
         
         return self.df
-            
-        
